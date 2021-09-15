4381bc5d09093fc93114238ee09778fc81cc3f11
rpiddocke <github@piddocke.com>
2021-09-14 20:00:56 -0700

    Assignment 1

--- Expressions Assignment/src/Main.java
+++ Expressions Assignment/src/Main.java
@@ -15,17 +15,37 @@ public class Main {
         */
         //Expression 1
         System.out.println((9 - 4)+ 10/2 - (4 - 8)/3);
+        System.out.println((5)+ 10/2 - (-4)/3);
+        System.out.println((5)+ 5 - (-4)/3);
+        System.out.println(10 + 1);
+        System.out.println(11);
+
         // My Answer
         //Expression 2
         System.out.println(23%4 + (18 - 3 / 2));
+        System.out.println(3 + 17);
+        System.out.println(20);
 
         //Expression 3
         System.out.println(((8 - 5) + Math.pow(3,4)/5));
+        System.out.println(((3) + 81/5));
+        System.out.println(((3) + 16.2));
+        System.out.println(19.2);
 
         //Expression 4
         System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+        System.out.println((9 % 4) - 8 + 8 / 8 + (9 / 3));
+        System.out.println((9 % 4) - 8 + 1 + 3);
+        System.out.println((9 % 4) - 4);
+        System.out.println(1 - 4);
 
         //Expression 5
         System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+        System.out.println(((14 / 2 + 3) * 0.8509035245341184 + 2) - Math.pow(5, 3));
+        System.out.println(((14 / 2 + 3) * 0.8509035245341184 + 2) - 125.0);
+        System.out.println((10 * 0.8509035245341184 + 2) - 125.0);
+        System.out.println(10.509035245341185 - 125.0);
+        System.out.println(-114.49096475465882);
+
     }
 }
